1d51566c9ae37331f7ca6d5f1e95222acbf66c54
LucasSantos27 <lucas-unifei@outlook.com>
2022-06-07 08:44:50 -0300

    feat: add plan field in user model

--- models/User.js
+++ models/User.js
@@ -2,39 +2,44 @@ const mongoose = require("../db/conn");
 const { Schema } = mongoose;
 
 const User = mongoose.model(
-    "User",
-    new Schema(
-        {
-            name: {
-                type: String,
-                required: true,
-            },
-            email: {
-                type: String,
-                required: true,
-            },
-            password: {
-                type: String,
-                required: true,
-            },
-            image: {
-                type: String,
-            },
-            cpf: {
-                type: String,
-                required: true,
-            },
-            birthday: {
-                type: Date,
-                required: true,
-            },
-            isAdmin: {
-                type: Boolean,
-                default: false,
-            },
-        },
-        { timestamps: true }
-    )
+  "User",
+  new Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      image: {
+        type: String,
+      },
+      cpf: {
+        type: String,
+        required: true,
+      },
+      birthday: {
+        type: Date,
+        required: true,
+      },
+      isAdmin: {
+        type: Boolean,
+        default: false,
+      },
+      plan: {
+        type: String,
+        enum: ["FREE", "MEDIUM", "PREMIUM"],
+        default: "FREE",
+      },
+    },
+    { timestamps: true }
+  )
 );
 
 module.exports = User;
